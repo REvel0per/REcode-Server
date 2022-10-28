f162c6e73dcbfba9539a502db1f05ca8a33e3f55
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-10-28 21:03:59 +0900

    change server to websocket

--- src/fileDto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class fileDto {
-  @ApiProperty({ description: 'filename' })
-  name!: string;
-
-  @ApiProperty({ description: 'content' })
-  body!: string;
-}
